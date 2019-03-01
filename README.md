a81cb2366fbe120d79f99b53a4e374c1e8b4ee89
laurent <laurent.mazare@gmail.com>
2019-03-01 23:27:00 +0000

    Install pre-built binaries when libtorch is not available.

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ Experimental rust bindings for PyTorch.
 The code generation part for the C api on top of libtorch comes from
 [ocaml-torch](https://github.com/LaurentMazare/ocaml-torch).
 
-## Instructions
+## Getting Started
+
+This crate requires the C++ version of PyTorch (libtorch) to be available on
+your system. You can either install it manually and let the build script now about
+it via the `LIBTORCH` environment variable. If not set, the build script will
+try downloading and extracting a pre-built binary version of libtorch.
+
+### Libtorch Manual Install
 
 - Get `libtorch` from the
 [PyTorch website download section](https://pytorch.org/get-started/locally/) and extract
--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
+// The LIBTORCH environment variable is used to locate the libtorch
+// library. If not set this build script will try downloading and
+// extracting a pre-built binary version of libtorch.
 use cmake::Config;
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn make(libtorch: &PathBuf, libtorch_lib: &PathBuf) {
     let libtorch_cxx11_abi = env::var("LIBTORCH_CXX11_ABI").unwrap_or("0".to_string());
@@ -28,9 +32,43 @@ fn cmake(libtorch: &PathBuf) {
     println!("cargo:rustc-link-lib=stdc++");
 }
 
+fn maybe_download(filename: &std::path::Path, url: &str) {
+    if !filename.exists() {
+        Command::new("wget")
+            .args(&[url, "-O", &filename.to_string_lossy().into_owned()])
+            .status().unwrap();
+    }
+}
+
 fn main() {
-    let libtorch = env::var("LIBTORCH").expect("LIBTORCH not defined");
-    let libtorch = PathBuf::from(libtorch);
+    let libtorch =
+        if let Ok(libtorch) = env::var("LIBTORCH") { PathBuf::from(libtorch) }
+        else {
+            let output = PathBuf::from(&env::var("OUT_DIR").unwrap());
+            let libtorch = output.join("libtorch");
+            if !libtorch.exists() {
+                let url = match env::consts::OS {
+                    "macos" => "https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.0.1.zip",
+                    "linux" => "https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.0.1.zip",
+                    otherwise => panic!("unsupported os {}", otherwise)
+                };
+                let filename = url.split("/").last().unwrap();
+                let download_dir = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("target");
+                if !download_dir.exists() {
+                    std::fs::create_dir(&download_dir).unwrap();
+                }
+                let absolute_filename = download_dir.join(filename);
+                println!("Downloading {}", url);
+                println!("To {:?}", absolute_filename);
+                maybe_download(&absolute_filename, &url);
+                println!("Extracting in {:?}", libtorch);
+                Command::new("unzip")
+                    .arg(&absolute_filename)
+                    .args(&["-d", &output.to_string_lossy().into_owned()])
+                    .status().unwrap();
+            }
+            libtorch
+        };
     let libtorch_lib = libtorch.join("lib");
     println!(
         "cargo:rustc-link-search=native={}",
