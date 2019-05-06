7060ab5bca4ab37e6ad517fcc3c0de383fbbd8ee
laurent <laurent.mazare@gmail.com>
2019-05-06 19:59:20 +0100

    Fix some unused tensor warnings.

--- src/vision/dataset.rs
+++ src/vision/dataset.rs
@@ -79,7 +79,7 @@ pub fn random_cutout(t: &Tensor, sz: i64) -> Tensor {
     for batch_index in 0..size[0] {
         let start_h = rand::thread_rng().gen_range(0, size[2] - sz + 1);
         let start_w = rand::thread_rng().gen_range(0, size[3] - sz + 1);
-        output
+        let _output = output
             .narrow(0, batch_index, 1)
             .narrow(2, start_h, sz)
             .narrow(3, start_w, sz)
