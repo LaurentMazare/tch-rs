335ce859fe960e22c505fc75979799a3dceed5f0
laurent <laurent.mazare@gmail.com>
2019-05-23 21:29:34 +0100

    Add distinct gpu and cpu mode.

--- examples/memory_test.rs
+++ examples/memory_test.rs
@@ -2,9 +2,16 @@ extern crate tch;
 use tch::Tensor;
 
 fn main() {
+    let a: Vec<String> = std::env::args().collect();
+    let device = match a.iter().map(|x| x.as_str()).collect::<Vec<_>>().as_slice() {
+        [_] => tch::Device::Cpu,
+        [_, "cpu"] => tch::Device::Cpu,
+        [_, "gpu"] => tch::Device::Cuda(0),
+        _ => panic!("usage: main cpu|gpu"),
+    };
     let slice = vec![0; 1_000_000];
     for i in 1..1_000_000 {
-        let t = Tensor::of_slice(&slice);
+        let t = Tensor::of_slice(&slice).to_device(device);
         println!("{} {:?}", i, t.size())
     }
 }
