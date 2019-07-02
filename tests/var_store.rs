cf7133e612f4d931c03b8f1a925755b624f50a98
laurent <laurent.mazare@gmail.com>
2019-07-02 22:20:24 +0100

    More formatting + run cargofmt in CI.

--- tests/var_store.rs
+++ tests/var_store.rs
@@ -1,14 +1,12 @@
-use tch::{Device, nn::VarStore};
+use tch::{nn::VarStore, Device};
 
 #[test]
 fn var_store_entry() {
     let vs = VarStore::new(Device::Cpu);
     let root = vs.root();
 
-    let t1 = root.entry("key")
-        .or_zeros(&[3, 1 , 4]);
-    let t2 = root.entry("key")
-        .or_zeros(&[1, 5 , 9]);
+    let t1 = root.entry("key").or_zeros(&[3, 1, 4]);
+    let t2 = root.entry("key").or_zeros(&[1, 5, 9]);
 
     assert_eq!(t1.size(), &[3, 1, 4]);
     assert_eq!(t2.size(), &[3, 1, 4]);
