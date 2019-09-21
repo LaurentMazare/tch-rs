24e4a3e87074effd64b41a81668abdfeb09e16de
laurent <laurent.mazare@gmail.com>
2019-09-21 19:00:15 +0100

    Move the var-store tests to their dedicated file.

--- tests/nn_tests.rs
+++ tests/nn_tests.rs
@@ -2,35 +2,6 @@ use tch::nn::{Module, OptimizerConfig};
 use tch::{kind, nn, Device, Kind, Reduction, Tensor};
 
 #[test]
-fn save_and_load_var_store() {
-    let filename = std::env::temp_dir().join(format!("tch-vs-{}", std::process::id()));
-    let add = |vs: &tch::nn::Path| {
-        let v = vs.sub("a").sub("b").ones("t2", &[3]);
-        let u = vs.zeros("t1", &[4]);
-        let _w = vs.sub("a").sub("b").sub("ccc").ones("t123", &[3]);
-        let _w = vs.sub("a").sub("b").sub("ccc").ones("t123", &[3]);
-        (u, v)
-    };
-    let vs1 = nn::VarStore::new(Device::Cpu);
-    let mut vs2 = nn::VarStore::new(Device::Cpu);
-    let (mut u1, mut v1) = add(&vs1.root());
-    let (u2, v2) = add(&vs2.root());
-    tch::no_grad(|| {
-        u1 += 42.0;
-        v1 *= 2.0;
-    });
-    assert_eq!(f64::from(&u1.mean(Kind::Float)), 42.0);
-    assert_eq!(f64::from(&v1.mean(Kind::Float)), 2.0);
-    assert_eq!(f64::from(&u2.mean(Kind::Float)), 0.0);
-    assert_eq!(f64::from(&v2.mean(Kind::Float)), 1.0);
-    vs1.save(&filename).unwrap();
-    vs2.load(&filename).unwrap();
-    assert_eq!(f64::from(&u1.mean(Kind::Float)), 42.0);
-    assert_eq!(f64::from(&u2.mean(Kind::Float)), 42.0);
-    assert_eq!(f64::from(&v2.mean(Kind::Float)), 2.0);
-}
-
-#[test]
 fn optimizer_test() {
     tch::manual_seed(42);
     // Create some linear data.
@@ -68,37 +39,6 @@ fn optimizer_test() {
     });
     let initial_loss2 = f64::from(xs.apply(&linear).mse_loss(&ys, Reduction::Mean));
     assert_eq!(initial_loss, initial_loss2)
-}
-
-#[test]
-fn var_store_test() {
-    let vs = nn::VarStore::new(Device::Cpu);
-    let zeros = vs.root().zeros("t1", &[3]);
-    assert_eq!(Vec::<f64>::from(&zeros), [0., 0., 0.]);
-    let zeros = vs.root().var("t2", &[3], nn::Init::Const(0.));
-    assert_eq!(Vec::<f64>::from(&zeros), [0., 0., 0.]);
-    let ones = vs.root().var("t3", &[3], nn::Init::Const(1.));
-    assert_eq!(Vec::<f64>::from(&ones), [1., 1., 1.]);
-    let forty_two = vs.root().var("t4", &[2], nn::Init::Const(42.));
-    assert_eq!(Vec::<f64>::from(&forty_two), [42., 42.]);
-    let uniform = vs
-        .root()
-        .var("t5", &[100], nn::Init::Uniform { lo: 1.0, up: 2.0 });
-    let uniform_min = f64::from(&uniform.min());
-    let uniform_max = f64::from(&uniform.max());
-    assert!(uniform_min >= 1., "min {}", uniform_min);
-    assert!(uniform_max <= 2., "max {}", uniform_max);
-    let uniform_std = f64::from(&uniform.std(true));
-    assert!(
-        uniform_std > 0.15 && uniform_std < 0.35,
-        "std {}",
-        uniform_std
-    );
-    let mut vs2 = nn::VarStore::new(Device::Cpu);
-    let ones = vs2.root().ones("t1", &[3]);
-    assert_eq!(Vec::<f64>::from(&ones), [1., 1., 1.]);
-    vs2.copy(&vs).unwrap();
-    assert_eq!(Vec::<f64>::from(&ones), [0., 0., 0.]);
 }
 
 fn my_module(p: nn::Path, dim: i64) -> impl nn::Module {
--- tests/var_store.rs
+++ tests/var_store.rs
@@ -1,4 +1,4 @@
-use tch::{nn::VarStore, Device};
+use tch::{nn::Init, nn::VarStore, Device, Kind};
 
 #[test]
 fn var_store_entry() {
@@ -10,4 +10,64 @@ fn var_store_entry() {
 
     assert_eq!(t1.size(), &[3, 1, 4]);
     assert_eq!(t2.size(), &[3, 1, 4]);
+}
+
+#[test]
+fn save_and_load_var_store() {
+    let filename = std::env::temp_dir().join(format!("tch-vs-{}", std::process::id()));
+    let add = |vs: &tch::nn::Path| {
+        let v = vs.sub("a").sub("b").ones("t2", &[3]);
+        let u = vs.zeros("t1", &[4]);
+        let _w = vs.sub("a").sub("b").sub("ccc").ones("t123", &[3]);
+        let _w = vs.sub("a").sub("b").sub("ccc").ones("t123", &[3]);
+        (u, v)
+    };
+    let vs1 = VarStore::new(Device::Cpu);
+    let mut vs2 = VarStore::new(Device::Cpu);
+    let (mut u1, mut v1) = add(&vs1.root());
+    let (u2, v2) = add(&vs2.root());
+    tch::no_grad(|| {
+        u1 += 42.0;
+        v1 *= 2.0;
+    });
+    assert_eq!(f64::from(&u1.mean(Kind::Float)), 42.0);
+    assert_eq!(f64::from(&v1.mean(Kind::Float)), 2.0);
+    assert_eq!(f64::from(&u2.mean(Kind::Float)), 0.0);
+    assert_eq!(f64::from(&v2.mean(Kind::Float)), 1.0);
+    vs1.save(&filename).unwrap();
+    vs2.load(&filename).unwrap();
+    assert_eq!(f64::from(&u1.mean(Kind::Float)), 42.0);
+    assert_eq!(f64::from(&u2.mean(Kind::Float)), 42.0);
+    assert_eq!(f64::from(&v2.mean(Kind::Float)), 2.0);
+}
+
+#[test]
+fn init_test() {
+    let vs = VarStore::new(Device::Cpu);
+    let zeros = vs.root().zeros("t1", &[3]);
+    assert_eq!(Vec::<f64>::from(&zeros), [0., 0., 0.]);
+    let zeros = vs.root().var("t2", &[3], Init::Const(0.));
+    assert_eq!(Vec::<f64>::from(&zeros), [0., 0., 0.]);
+    let ones = vs.root().var("t3", &[3], Init::Const(1.));
+    assert_eq!(Vec::<f64>::from(&ones), [1., 1., 1.]);
+    let forty_two = vs.root().var("t4", &[2], Init::Const(42.));
+    assert_eq!(Vec::<f64>::from(&forty_two), [42., 42.]);
+    let uniform = vs
+        .root()
+        .var("t5", &[100], Init::Uniform { lo: 1.0, up: 2.0 });
+    let uniform_min = f64::from(&uniform.min());
+    let uniform_max = f64::from(&uniform.max());
+    assert!(uniform_min >= 1., "min {}", uniform_min);
+    assert!(uniform_max <= 2., "max {}", uniform_max);
+    let uniform_std = f64::from(&uniform.std(true));
+    assert!(
+        uniform_std > 0.15 && uniform_std < 0.35,
+        "std {}",
+        uniform_std
+    );
+    let mut vs2 = VarStore::new(Device::Cpu);
+    let ones = vs2.root().ones("t1", &[3]);
+    assert_eq!(Vec::<f64>::from(&ones), [1., 1., 1.]);
+    vs2.copy(&vs).unwrap();
+    assert_eq!(Vec::<f64>::from(&ones), [0., 0., 0.]);
 }
