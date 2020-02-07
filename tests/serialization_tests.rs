359e7950f934e6a39bdf2f5a0edc29dedbf46260
laurent <laurent.mazare@gmail.com>
2020-02-07 20:49:06 +0000

    Add a test for npz serialization.

---
+++ tests/serialization_tests.rs
@@ -0,0 +1,37 @@
+use tch::Tensor;
+
+#[test]
+fn save_and_load() {
+    let filename = std::env::temp_dir().join(format!("tch-{}", std::process::id()));
+    let vec = [3.0, 1.0, 4.0, 1.0, 5.0].to_vec();
+    let t1 = Tensor::of_slice(&vec);
+    t1.save(&filename).unwrap();
+    let t2 = Tensor::load(&filename).unwrap();
+    assert_eq!(Vec::<f64>::from(&t2), vec)
+}
+
+#[test]
+fn save_and_load_multi() {
+    let filename = std::env::temp_dir().join(format!("tch2-{}", std::process::id()));
+    let pi = Tensor::of_slice(&[3.0, 1.0, 4.0, 1.0, 5.0]);
+    let e = Tensor::of_slice(&[2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 6]);
+    Tensor::save_multi(&[(&"pi", &pi), (&"e", &e)], &filename).unwrap();
+    let named_tensors = Tensor::load_multi(&filename).unwrap();
+    assert_eq!(named_tensors.len(), 2);
+    assert_eq!(named_tensors[0].0, "pi");
+    assert_eq!(named_tensors[1].0, "e");
+    assert_eq!(i64::from(&named_tensors[1].1.sum(tch::Kind::Float)), 57);
+}
+
+#[test]
+fn save_and_load_npz() {
+    let filename = std::env::temp_dir().join(format!("tch3-{}.npz", std::process::id()));
+    let pi = Tensor::of_slice(&[3.0, 1.0, 4.0, 1.0, 5.0]);
+    let e = Tensor::of_slice(&[2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 6]);
+    Tensor::write_npz(&[(&"pi", &pi), (&"e", &e)], &filename).unwrap();
+    let named_tensors = Tensor::read_npz(&filename).unwrap();
+    assert_eq!(named_tensors.len(), 2);
+    assert_eq!(named_tensors[0].0, "pi");
+    assert_eq!(named_tensors[1].0, "e");
+    assert_eq!(i64::from(&named_tensors[1].1.sum(tch::Kind::Float)), 57);
+}
--- tests/tensor_tests.rs
+++ tests/tensor_tests.rs
@@ -111,29 +111,6 @@ fn cat_and_stack() {
 }
 
 #[test]
-fn save_and_load() {
-    let filename = std::env::temp_dir().join(format!("tch-{}", std::process::id()));
-    let vec = [3.0, 1.0, 4.0, 1.0, 5.0].to_vec();
-    let t1 = Tensor::of_slice(&vec);
-    t1.save(&filename).unwrap();
-    let t2 = Tensor::load(&filename).unwrap();
-    assert_eq!(Vec::<f64>::from(&t2), vec)
-}
-
-#[test]
-fn save_and_load_multi() {
-    let filename = std::env::temp_dir().join(format!("tch2-{}", std::process::id()));
-    let pi = Tensor::of_slice(&[3.0, 1.0, 4.0, 1.0, 5.0]);
-    let e = Tensor::of_slice(&[2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 6]);
-    Tensor::save_multi(&[(&"pi", &pi), (&"e", &e)], &filename).unwrap();
-    let named_tensors = Tensor::load_multi(&filename).unwrap();
-    assert_eq!(named_tensors.len(), 2);
-    assert_eq!(named_tensors[0].0, "pi");
-    assert_eq!(named_tensors[1].0, "e");
-    assert_eq!(i64::from(&named_tensors[1].1.sum(tch::Kind::Float)), 57);
-}
-
-#[test]
 fn onehot() {
     let xs = Tensor::of_slice(&[0, 1, 2, 3]);
     let onehot = xs.onehot(4);
