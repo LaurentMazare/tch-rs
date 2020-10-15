30e5f8e86ac48c969d028ce6a4a9a52bedf90f85
laurent <laurent.mazare@gmail.com>
2020-10-15 20:08:21 +0100

    Use c_char rather than i8 (bis).

--- src/wrappers/jit.rs
+++ src/wrappers/jit.rs
@@ -307,7 +307,7 @@ impl CModule {
     ) -> Result<CModule, TchError> {
         let mut buffer = Vec::new();
         f.read_to_end(&mut buffer)?;
-        let buffer_ptr = buffer.as_ptr() as *const i8;
+        let buffer_ptr = buffer.as_ptr() as *const libc::c_char;
         let c_module = unsafe_torch_err!(atm_load_str_on_device(
             buffer_ptr,
             buffer.len(),
