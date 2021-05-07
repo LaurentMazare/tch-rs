37aae3b7be1f78b385c43f8d99372aab7c33d39e
Johan Verwey <joverwey@microsoft.com>
2021-05-07 09:36:33 -0700

    Fix build on windows for PyTorch 1.8.1
    On inspection of torch_cuda_cu.lib using
    `dumpbin`, the following symbol shows up:
    
                      ?getCurrentCUDABlasHandle@cuda@at@@YAPEAUcublasContext@@XZ (struct cublasContext * __cdecl at::cuda::getCurrentCUDABlasHandle(void))
    
    The return type is a struct cublasContext* but in dummy_cuda_dependency.cpp it was declared as void which causes a link error on Windows.
    
    I haven't tested this on Linux since I don't have a working CUDA version for WSL. Could someone verify that this still works cross platform?

--- torch-sys/libtch/dummy_cuda_dependency.cpp
+++ torch-sys/libtch/dummy_cuda_dependency.cpp
@@ -5,9 +5,15 @@ extern "C" {
     void dummy_cuda_dependency();
 }
 
+struct cublasContext;
+
 namespace at {
+    class Tensor;
+    namespace native {
+        class at::Tensor searchsorted_cuda(class at::Tensor const &,class at::Tensor const &,bool,bool);
+    }
     namespace cuda {
-        void *getCurrentCUDABlasHandle();
+        cublasContext* getCurrentCUDABlasHandle();
         int warp_size();
     }
 }
