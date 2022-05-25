1f233800bbe17119bc0c139bfebb63958f92c540
CalvinCoolade <7358618-CalvinCoolade@users.noreply.gitlab.com>
2022-05-25 10:51:44 -0400

    correct the other typo

--- src/nn/var_store.rs
+++ src/nn/var_store.rs
@@ -652,7 +652,7 @@ impl<'a> Path<'a> {
         self.f_kaiming_uniform(name, dims).unwrap()
     }
 
-    /// Creats a new variable initialized randomly with an orthogonal matrix
+    /// Creates a new variable initialized randomly with an orthogonal matrix
     ///
     /// The new variable is named according to the name parameter and
     /// has the specified shape. The variable is trainable, its gradient
