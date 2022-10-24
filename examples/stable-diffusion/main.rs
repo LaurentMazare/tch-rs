e2200360b33007057769e442b77657589f7836c3
laurent <laurent.mazare@gmail.com>
2022-10-24 21:43:12 +0100

    Resnet block.

--- examples/stable-diffusion/main.rs
+++ examples/stable-diffusion/main.rs
@@ -671,12 +671,77 @@ impl Upsample2D {
     }
 }
 
+struct ResnetBlock2DConfig {
+    out_channels: Option<i64>,
+    conv_shortcut: bool,
+    groups: i64,
+    groups_out: Option<i64>,
+    pre_norm: bool,
+    eps: f64,
+    use_in_shortcut: Option<bool>,
+    // non_linearity: silu
+    output_scale_factor: f64,
+}
+
+impl Default for ResnetBlock2DConfig {
+    fn default() -> Self {
+        Self {
+            out_channels: None,
+            conv_shortcut: false,
+            groups: 32,
+            groups_out: None,
+            pre_norm: true,
+            eps: 1e-6,
+            use_in_shortcut: None,
+            output_scale_factor: 1.,
+        }
+    }
+}
+
 struct ResnetBlock2D {
     norm1: nn::GroupNorm,
     conv1: nn::Conv2D,
     norm2: nn::GroupNorm,
     conv2: nn::Conv2D,
     conv_shortcut: Option<nn::Conv2D>,
+    use_in_shortcut: bool,
+    config: ResnetBlock2DConfig,
+}
+
+impl ResnetBlock2D {
+    fn new(vs: nn::Path, in_channels: i64, config: ResnetBlock2DConfig) -> Self {
+        let out_channels = config.out_channels.unwrap_or(in_channels);
+        let conv_cfg = nn::ConvConfig { stride: 1, padding: 1, ..Default::default() };
+        let group_cfg = nn::GroupNormConfig { eps: config.eps, affine: true, ..Default::default() };
+        let norm1 = nn::group_norm(&vs / "norm1", config.groups, in_channels, group_cfg);
+        let conv1 = nn::conv2d(&vs / "conv1", in_channels, out_channels, 3, conv_cfg);
+        let groups_out = config.groups_out.unwrap_or(config.groups);
+        let norm2 = nn::group_norm(&vs / "norm2", groups_out, out_channels, group_cfg);
+        let conv2 = nn::conv2d(&vs / "conv2", out_channels, out_channels, 3, conv_cfg);
+        let use_in_shortcut = config.use_in_shortcut.unwrap_or(in_channels != out_channels);
+        let conv_shortcut = if use_in_shortcut {
+            let conv_cfg = nn::ConvConfig { stride: 1, padding: 0, ..Default::default() };
+            Some(nn::conv2d(&vs / "conv_shortcut", in_channels, out_channels, 1, conv_cfg))
+        } else {
+            None
+        };
+        Self { norm1, conv1, norm2, conv2, config, conv_shortcut, use_in_shortcut }
+    }
+
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let shortcut_xs = match &self.conv_shortcut {
+            Some(conv_shortcut) => xs.apply(conv_shortcut),
+            None => xs.shallow_clone(),
+        };
+        let xs = xs
+            .apply(&self.norm1)
+            .silu()
+            .apply(&self.conv1)
+            .apply(&self.norm2)
+            .silu()
+            .apply(&self.conv2);
+        (shortcut_xs + xs) / self.config.output_scale_factor
+    }
 }
 
 struct DownEncoderBlock2D {
