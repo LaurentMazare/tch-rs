511e9f8d3dec16bf22f35e3ca4c9a06af8a50e01
laurent <laurent.mazare@gmail.com>
2022-10-26 05:54:17 +0100

    Implement AutoEncoderKL.

--- examples/stable-diffusion/main.rs
+++ examples/stable-diffusion/main.rs
@@ -1170,12 +1170,109 @@ impl Module for Encoder {
     }
 }
 
+#[derive(Debug, Clone)]
+struct DecoderConfig {
+    // up_block_types: UpDecoderBlock2D
+    block_out_channels: Vec<i64>,
+    layers_per_block: i64,
+    norm_num_groups: i64,
+}
+
+impl Default for DecoderConfig {
+    fn default() -> Self {
+        Self { block_out_channels: vec![64], layers_per_block: 2, norm_num_groups: 32 }
+    }
+}
+
 #[derive(Debug)]
 struct Decoder {
     conv_in: nn::Conv2D,
     up_blocks: Vec<UpDecoderBlock2D>,
+    mid_block: UNetMidBlock2D,
     conv_norm_out: nn::GroupNorm,
     conv_out: nn::Conv2D,
+    config: DecoderConfig,
+}
+
+impl Decoder {
+    fn new(vs: nn::Path, in_channels: i64, out_channels: i64, config: DecoderConfig) -> Self {
+        let n_block_out_channels = config.block_out_channels.len();
+        let last_block_out_channels = *config.block_out_channels.last().unwrap();
+        let conv_cfg = nn::ConvConfig { stride: 1, padding: 1, ..Default::default() };
+        let conv_in =
+            nn::conv2d(&vs / "conv_in", in_channels, last_block_out_channels, 3, conv_cfg);
+        let mid_cfg = UNetMidBlock2DConfig {
+            resnet_eps: 1e-6,
+            output_scale_factor: 1.,
+            attn_num_head_channels: None,
+            resnet_groups: Some(config.norm_num_groups),
+            ..Default::default()
+        };
+        let mid_block = UNetMidBlock2D::new(&vs / "mid_block", last_block_out_channels, mid_cfg);
+        let mut up_blocks = vec![];
+        let vs_up_blocks = &vs / "up_blocks";
+        let reversed_block_out_channels: Vec<_> =
+            config.block_out_channels.iter().map(|x| *x).rev().collect();
+        for index in 0..n_block_out_channels {
+            let out_channels = reversed_block_out_channels[index];
+            let in_channels = if index > 0 {
+                reversed_block_out_channels[index - 1]
+            } else {
+                reversed_block_out_channels[0]
+            };
+            let is_final = index + 1 == n_block_out_channels;
+            let cfg = UpDecoderBlock2DConfig {
+                num_layers: config.layers_per_block + 1,
+                resnet_eps: 1e-6,
+                resnet_groups: config.norm_num_groups,
+                add_upsample: !is_final,
+                ..Default::default()
+            };
+            let up_block =
+                UpDecoderBlock2D::new(&vs_up_blocks / index, in_channels, out_channels, cfg);
+            up_blocks.push(up_block)
+        }
+        let group_cfg = nn::GroupNormConfig { eps: 1e-6, ..Default::default() };
+        let conv_norm_out = nn::group_norm(
+            &vs / "conv_norm_out",
+            config.norm_num_groups,
+            config.block_out_channels[0],
+            group_cfg,
+        );
+        let conv_cfg = nn::ConvConfig { padding: 1, ..Default::default() };
+        let conv_out =
+            nn::conv2d(&vs / "conv_out", config.block_out_channels[0], out_channels, 3, conv_cfg);
+        Self { conv_in, up_blocks, mid_block, conv_norm_out, conv_out, config }
+    }
+}
+
+impl Module for Decoder {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let mut xs = xs.apply(&self.conv_in).apply(&self.mid_block);
+        for up_block in self.up_blocks.iter() {
+            xs = xs.apply(up_block)
+        }
+        xs.apply(&self.conv_norm_out).silu().apply(&self.conv_out)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct AutoEncoderKLConfig {
+    block_out_channels: Vec<i64>,
+    layers_per_block: i64,
+    latent_channels: i64,
+    norm_num_groups: i64,
+}
+
+impl Default for AutoEncoderKLConfig {
+    fn default() -> Self {
+        Self {
+            block_out_channels: vec![64],
+            layers_per_block: 1,
+            latent_channels: 4,
+            norm_num_groups: 32,
+        }
+    }
 }
 
 // https://github.com/huggingface/diffusers/blob/970e30606c2944e3286f56e8eb6d3dc6d1eb85f7/src/diffusers/models/vae.py#L485
@@ -1187,9 +1284,46 @@ struct AutoEncoderKL {
     decoder: Decoder,
     quant_conv: nn::Conv2D,
     post_quant_conv: nn::Conv2D,
+    config: AutoEncoderKLConfig,
+}
+
+impl AutoEncoderKL {
+    fn new(vs: nn::Path, in_channels: i64, out_channels: i64, config: AutoEncoderKLConfig) -> Self {
+        let latent_channels = config.latent_channels;
+        let encoder_cfg = EncoderConfig {
+            block_out_channels: config.block_out_channels.clone(),
+            layers_per_block: config.layers_per_block,
+            norm_num_groups: config.norm_num_groups,
+            double_z: true,
+        };
+        let encoder = Encoder::new(&vs / "encoder", in_channels, latent_channels, encoder_cfg);
+        let decoder_cfg = DecoderConfig {
+            block_out_channels: config.block_out_channels.clone(),
+            layers_per_block: config.layers_per_block,
+            norm_num_groups: config.norm_num_groups,
+        };
+        let decoder = Decoder::new(&vs / "decoder", latent_channels, out_channels, decoder_cfg);
+        let conv_cfg = Default::default();
+        let quant_conv =
+            nn::conv2d(&vs / "quant_conv", 2 * latent_channels, 2 * latent_channels, 1, conv_cfg);
+        let post_quant_conv =
+            nn::conv2d(&vs / "post_quant_conv", latent_channels, latent_channels, 1, conv_cfg);
+        Self { encoder, decoder, quant_conv, post_quant_conv, config }
+    }
+
+    // Returns the parameters of the latent distribution.
+    fn encode(&self, xs: &Tensor) -> Tensor {
+        xs.apply(&self.encoder).apply(&self.quant_conv)
+    }
+
+    /// Takes as input some sampled values.
+    fn decode(&self, xs: &Tensor) -> Tensor {
+        xs.apply(&self.post_quant_conv).apply(&self.decoder)
+    }
 }
 
 // TODO: UNet2DConditionModel
+// https://huggingface.co/CompVis/stable-diffusion-v1-4/blob/main/vae/config.json
 // https://github.com/huggingface/diffusers/blob/970e30606c2944e3286f56e8eb6d3dc6d1eb85f7/src/diffusers/models/unet_2d_condition.py#L37
 // TODO: LMSDiscreteScheduler
 // https://github.com/huggingface/diffusers/blob/32bf4fdc4386809c870528cb261028baae012d27/src/diffusers/schedulers/scheduling_lms_discrete.py#L47
