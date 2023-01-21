1ede195d41c110cd7f55b597d46bd1fea30dbfb4
Aspect <ttmmiizz100@gmail.com>
2023-01-20 21:11:38 -0500

    Fix typo

--- src/wrappers/tensor.rs
+++ src/wrappers/tensor.rs
@@ -831,7 +831,7 @@ pub struct NoGradGuard {
 /// Disables gradient tracking, this will be enabled back when the
 /// returned value gets deallocated.
 /// Note that it is important to bind this to a name like "_guard"
-/// and not to "_" as these two would have different semantics.
+/// and not to "\_" as these two would have different semantics.
 /// See https://internals.rust-lang.org/t/pre-rfc-must-bind/12658/46
 /// for more details.
 pub fn no_grad_guard() -> NoGradGuard {
