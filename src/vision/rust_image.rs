d302285a21d429ee23c9ca976b1d4272b1563ec1
laurent <laurent.mazare@gmail.com>
2023-03-11 10:30:38 +0000

    Minor cleanups.

--- src/vision/rust_image.rs
+++ src/vision/rust_image.rs
@@ -1,3 +1,4 @@
+/// Conversion helpers for the image crate.
 use std::convert::TryFrom;
 
 use image::{
@@ -17,58 +18,50 @@ impl<'i> TryFrom<&'i DynamicImage> for Tensor {
             DynamicImage::ImageLumaA8(gray_a) => Tensor::try_from(gray_a),
             DynamicImage::ImageRgb8(rgb) => Tensor::try_from(rgb),
             DynamicImage::ImageRgba8(rgba) => Tensor::try_from(rgba),
-            DynamicImage::ImageLuma16(_) => unimplemented!(),
-            DynamicImage::ImageLumaA16(_) => unimplemented!(),
-            DynamicImage::ImageRgb16(_) => unimplemented!(),
-            DynamicImage::ImageRgba16(_) => unimplemented!(),
             DynamicImage::ImageRgb32F(rgb) => Tensor::try_from(rgb),
             DynamicImage::ImageRgba32F(rgba) => Tensor::try_from(rgba),
-            _ => {
-                unimplemented!()
-            }
+            _ => Err(TchError::Convert("unsupported DynamicImage variant".to_string())),
         }
     }
 }
 
 impl<'i> TryFrom<&'i GrayImage> for Tensor {
     type Error = TchError;
+
     ///  `h * w` => `1 * 1 * h * w`
     fn try_from(gray: &'i GrayImage) -> Result<Self, Self::Error> {
-        let kind = Kind::Uint8;
         let size = &[gray.height() as i64, gray.width() as i64, 1];
-        let tensor = Tensor::f_of_data_size(gray.as_bytes(), size, kind)?;
+        let tensor = Tensor::f_of_data_size(gray.as_bytes(), size, Kind::Uint8)?;
         Ok(hwc_to_chw(&tensor))
     }
 }
 
 impl<'i> TryFrom<&'i GrayAlphaImage> for Tensor {
     type Error = TchError;
+
     ///  `2 * h * w` => `1 * 2 * h * w`
     fn try_from(gray: &'i GrayAlphaImage) -> Result<Self, Self::Error> {
-        let kind = Kind::Uint8;
         let size = &[gray.height() as i64, gray.width() as i64, 2];
-        let tensor = Tensor::f_of_data_size(gray.as_bytes(), size, kind)?;
+        let tensor = Tensor::f_of_data_size(gray.as_bytes(), size, Kind::Uint8)?;
         Ok(hwc_to_chw(&tensor))
     }
 }
 
 impl<'i> TryFrom<&'i RgbImage> for Tensor {
-    /// `h * w * 3` => `1 * 3 * h * w`
     type Error = TchError;
 
+    /// `h * w * 3` => `1 * 3 * h * w`
     fn try_from(rgb: &'i RgbImage) -> Result<Self, Self::Error> {
-        let kind = Kind::Uint8;
         let size = &[rgb.height() as i64, rgb.width() as i64, 3];
-        let tensor = Tensor::f_of_data_size(rgb.as_raw(), size, kind)?;
+        let tensor = Tensor::f_of_data_size(rgb.as_raw(), size, Kind::Uint8)?;
         Ok(hwc_to_chw(&tensor))
     }
 }
 
 impl<'i> TryFrom<&'i RgbaImage> for Tensor {
-    /// `h * w * 4` => `1 * 4 * h * w`
-
     type Error = TchError;
 
+    /// `h * w * 4` => `1 * 4 * h * w`
     fn try_from(rgb: &'i RgbaImage) -> Result<Self, Self::Error> {
         let kind = Kind::Uint8;
         let size = &[rgb.height() as i64, rgb.width() as i64, 4];
@@ -79,6 +72,7 @@ impl<'i> TryFrom<&'i RgbaImage> for Tensor {
 
 impl<'i> TryFrom<&'i Tensor> for RgbImage {
     type Error = TchError;
+
     ///  `1 * 3 * h * w` => `h * w * 3`
     fn try_from(value: &'i Tensor) -> Result<Self, Self::Error> {
         let tensor = assert_tensor_as_image(value, Kind::Uint8, 3)?;
@@ -87,16 +81,15 @@ impl<'i> TryFrom<&'i Tensor> for RgbImage {
         let length = (width * height * 3) as usize;
         let mut buffer = vec![0; length];
         tensor.f_copy_data(&mut buffer, length)?;
-        match RgbImage::from_raw(width, height, buffer) {
-            Some(s) => Ok(s),
-            None => Err(TchError::Convert("Failed to convert tensor to image".to_string())),
-        }
+        RgbImage::from_raw(width, height, buffer)
+            .ok_or_else(|| TchError::Convert("Failed to convert tensor to image".to_string()))
     }
 }
 
 impl<'i> TryFrom<&'i Rgb32FImage> for Tensor {
-    /// `h * w * 3` => `1 * 3 * h * w`
     type Error = TchError;
+
+    /// `h * w * 3` => `1 * 3 * h * w`
     fn try_from(rgb: &'i Rgb32FImage) -> Result<Self, Self::Error> {
         let kind = Kind::Float;
         let size = &[rgb.height() as i64, rgb.width() as i64, 3];
@@ -106,9 +99,9 @@ impl<'i> TryFrom<&'i Rgb32FImage> for Tensor {
 }
 
 impl<'i> TryFrom<&'i Rgba32FImage> for Tensor {
-    /// `h * w * 4` => `1 * 4 * h * w`
     type Error = TchError;
 
+    /// `h * w * 4` => `1 * 4 * h * w`
     fn try_from(rgb: &'i Rgba32FImage) -> Result<Self, Self::Error> {
         let kind = Kind::Float;
         let size = &[rgb.height() as i64, rgb.width() as i64, 4];
@@ -119,6 +112,7 @@ impl<'i> TryFrom<&'i Rgba32FImage> for Tensor {
 
 impl<'i> TryFrom<&'i Tensor> for Rgb32FImage {
     type Error = TchError;
+
     ///  `1 * 3 * h * w` => `h * w * 3`
     fn try_from(value: &'i Tensor) -> Result<Self, Self::Error> {
         let tensor = assert_tensor_as_image(value, Kind::Float, 3)?;
@@ -127,10 +121,8 @@ impl<'i> TryFrom<&'i Tensor> for Rgb32FImage {
         let length = (width * height * 3) as usize;
         let mut buffer = vec![0.0; length];
         tensor.f_copy_data(&mut buffer, length)?;
-        match Rgb32FImage::from_raw(width, height, buffer) {
-            Some(s) => Ok(s),
-            None => Err(TchError::Convert("Failed to convert tensor to image".to_string())),
-        }
+        Rgb32FImage::from_raw(width, height, buffer)
+            .ok_or_else(|| TchError::Convert("Failed to convert tensor to image".to_string()))
     }
 }
 
@@ -140,15 +132,14 @@ fn assert_tensor_as_image(tensor: &Tensor, except: Kind, channel: i64) -> Result
     let size = tensor.size();
     if size.len() != 3 {
         let msg = format!("Tensor size is `{size:?}`, except rank 3 tensor");
-        Err(TchError::Convert(msg))?;
-    }
-    if size[0] != channel {
+        Err(TchError::Convert(msg))
+    } else if size[0] != channel {
         let msg = format!("Tensor size is `{size:?}`, except {channel} channels");
-        Err(TchError::Convert(msg))?;
-    }
-    if kind != except {
+        Err(TchError::Convert(msg))
+    } else if kind != except {
         let msg = format!("Tensor kind is `{kind:?}`, except `{except:?}` tensor");
-        Err(TchError::Convert(msg))?;
+        Err(TchError::Convert(msg))
+    } else {
+        Ok(chw_to_hwc(tensor))
     }
-    Ok(chw_to_hwc(tensor))
 }
