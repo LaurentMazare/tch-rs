LaurentMazare/tch-rs

commit e7ee8166e2ee234c3863033916ce8f00bc76558e

Change Linear::forward() to match pytorch (#666)
* Change Linear::forward() to match pytorch

The current implementation of `Linear::forward()` is valid and correct; however it produces outputs
that are similar but not numerically identical to `torch.nn.Linear` in PyTorch. This commit replaces
the implementation with `Tensor::linear()`, which matches the PyTorch outputs exactly.

A Python script is included which generates a random linear layer and input tensor, then writes the
tensors and their expected output to disk. A Rust unit test then confirms that `tch-rs` produces the
same output. The test is marked `#[ignore]`, because the tensors themselves are not committed (they
would be about 3M, which feels a little large; if it's fine then they can be committed).

* Reduce size of test tensors, so they're small enough to commit.

These are the smallest tensors I could find that reproduce the difference in output between matmul()
and forward(). This commit also removes the `#[ignore]` attribute, because the test tensors are
small enough now that they can be committed to the repo.

src/nn/linear.rs
@@ -46,10 +46,27 @@ pub fn linear<'a, T: Borrow<super::Path<'a>>>(
 
 impl super::module::Module for Linear {
     fn forward(&self, xs: &Tensor) -> Tensor {
-        if let Some(bias) = &self.bs {
-            xs.matmul(&self.ws.tr()) + bias
-        } else {
-            xs.matmul(&self.ws.tr())
-        }
+        xs.linear(&self.ws, self.bs.as_ref())
     }
+}
+
+#[test]
+fn matches_pytorch() {
+    use crate::nn::Module;
+
+    let input = Tensor::read_npy("tests/linear/in.npy").unwrap();
+    let expected_output = Tensor::read_npy("tests/linear/out.npy").unwrap();
+    let ws = Tensor::read_npy("tests/linear/ws.npy").unwrap();
+    let bs = Some(Tensor::read_npy("tests/linear/bs.npy").unwrap());
+
+    let original_output =
+        if let Some(bias) = &bs { input.matmul(&ws.tr()) + bias } else { input.matmul(&ws.tr()) };
+
+    let linear = Linear { ws, bs };
+    let output = linear.forward(&input);
+
+    // The `matmul()` implementation close, but not as close as calling `linear()`
+    assert!(original_output.allclose(&expected_output, 1e-2, 1e-3, false));
+    assert!(!original_output.equal(&expected_output));
+    assert!(output.equal(&expected_output));
 }

tests/linear/gen-test-data.py
@@ -1,0 +1,16 @@
+# This generates some test weights for a linear layer, as well as a sample input/output pair. This
+# is used to demonstrate that the implementing a linear layer with `Tensor::matmul()` produces
+# outputs that are close to, but not exactly equal to, the outputs produced by `Tensor::linear()`.
+#
+# The tensor sizes chosen are as small as I could make them while still reproducing this issue.
+
+import numpy as np
+import torch
+
+linear = torch.nn.Linear(257, 4)
+input = torch.rand(2, 257)
+
+np.save('ws.npy', linear.weight.data)
+np.save('bs.npy', linear.bias.data)
+np.save('in.npy', input.data)
+np.save('out.npy', linear(input).data)

commit eb702f9079c42e72d9490b221fa977e08b12cde9

Rework linear test to not expect exact equality.
Proposed follow up to the CI failure introduced by https://github.com/LaurentMazare/tch-rs/pull/666

The test using `Tensor::equal()` worked on _my_ machine, but failed in CI. This version of the test
asserts that the implementation using `linear()` produces output that is `allclose()` to the
expected output, and also asserts that the distance to the expected output is smaller for the
`linear()` implementation than for the original `matmul()` implementation.

src/nn/linear.rs
@@ -65,8 +65,10 @@ fn matches_pytorch() {
     let linear = Linear { ws, bs };
     let output = linear.forward(&input);
 
-    // The `matmul()` implementation close, but not as close as calling `linear()`
-    assert!(original_output.allclose(&expected_output, 1e-2, 1e-3, false));
-    assert!(!original_output.equal(&expected_output));
-    assert!(output.equal(&expected_output));
+    let delta_output: f32 = (&output - &expected_output).norm().into();
+    let delta_original: f32 = (&original_output - &expected_output).norm().into();
+
+    // The `matmul()` implementation is close, but `linear()` is at least as close or closer.
+    assert!(output.allclose(&expected_output, 1e-5, 1e-8, false));
+    assert!(delta_output <= delta_original);
 }