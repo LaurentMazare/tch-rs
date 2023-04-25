d76181c4a2c9aa6caab4191439845289eb5521c6
laurent <laurent.mazare@gmail.com>
2023-04-25 20:47:56 +0100

    Refactor the efficientnet models.

--- src/vision/export_model.py
+++ src/vision/export_model.py
@@ -1,11 +1,16 @@
-# This script exports pre-trained model weights in numpy format.
-# These weights can then be converted to the libtorch native format via:
-# cargo run --example tensor-tools -- cp resnet18.npz resnet18.ot
+# This script exports pre-trained model weights in the safetensors format.
 import numpy as np
 import torch
 import torchvision
+from safetensors import torch as stt
 
 m = torchvision.models.efficientnet_b0(pretrained=True)
-nps = {}
-for k, v in m.state_dict().items(): nps[k] = v.numpy()
-np.savez('efficientnet-b0.npz', **nps)
+stt.save_file(m.state_dict(), 'efficientnet-b0.safetensors')
+m = torchvision.models.efficientnet_b1(pretrained=True)
+stt.save_file(m.state_dict(), 'efficientnet-b1.safetensors')
+m = torchvision.models.efficientnet_b2(pretrained=True)
+stt.save_file(m.state_dict(), 'efficientnet-b2.safetensors')
+m = torchvision.models.efficientnet_b3(pretrained=True)
+stt.save_file(m.state_dict(), 'efficientnet-b3.safetensors')
+m = torchvision.models.efficientnet_b4(pretrained=True)
+stt.save_file(m.state_dict(), 'efficientnet-b4.safetensors')
