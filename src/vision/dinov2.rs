146e443ff6c253a1a5956699a00c4e5bd421f68f
laurent <laurent.mazare@gmail.com>
2023-04-29 20:12:06 +0100

    More dino-v2.

--- src/vision/dinov2.rs
+++ src/vision/dinov2.rs
@@ -1,6 +1,9 @@
 //! DINOv2: Learning Robust Visual Features without Supervision
 //! https://github.com/facebookresearch/dinov2
-use crate::{nn, Kind, Result, Tensor};
+use crate::{nn, IndexOp, Kind, Tensor};
+
+const IMG_SIZE: i64 = 224;
+const PATCH_SIZE: i64 = 16;
 
 #[derive(Debug)]
 struct Attention {
@@ -26,7 +29,7 @@ impl nn::Module for Attention {
         let (b, n, c) = xs.size3().unwrap();
         let qkv = self
             .qkv
-            .forward(&xs)
+            .forward(xs)
             .reshape([b, n, 3, self.num_heads, c / self.num_heads])
             .permute([2, 0, 3, 1, 4]);
         let q = qkv.get(0) * self.scale;
@@ -106,13 +109,101 @@ impl nn::Module for Block {
     }
 }
 
+#[derive(Debug)]
+struct PatchEmbed {
+    proj: nn::Conv2D,
+    patch_size: (i64, i64),
+    num_patches: i64,
+}
+
+impl PatchEmbed {
+    fn new(vs: nn::Path, img_size: i64, patch_size: i64, in_chans: i64, embed_dim: i64) -> Self {
+        let config = nn::ConvConfig { stride: patch_size, ..Default::default() };
+        let proj = nn::conv2d(vs / "proj", in_chans, embed_dim, patch_size, config);
+        let num_patches = (img_size / patch_size) * (img_size / patch_size);
+        Self { proj, patch_size: (patch_size, patch_size), num_patches }
+    }
+}
+
+impl nn::Module for PatchEmbed {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let (_b, _c, h, w) = xs.size4().unwrap();
+        let (patch_h, patch_w) = self.patch_size;
+        if (h % patch_h) != 0 {
+            panic!("image height {h} is not a multiple of patch height {patch_h}")
+        }
+        if (w % patch_w) != 0 {
+            panic!("image width {w} is not a multiple of patch width {patch_w}")
+        }
+        let xs = xs.apply(&self.proj);
+        let (b, c, h, w) = xs.size4().unwrap();
+        // flatten embeddings.
+        xs.reshape([b, c, h * w]).transpose(1, 2)
+    }
+}
+
+#[derive(Debug)]
 pub struct DinoVisionTransformer {
+    patch_embed: PatchEmbed,
+    cls_token: Tensor,
+    pos_embed: Tensor,
+    blocks: Vec<Block>,
+    norm: nn::LayerNorm,
     head: nn::Linear,
 }
 
 impl DinoVisionTransformer {
-    pub fn new(vs: nn::Path) -> Self {
+    pub fn new(vs: nn::Path, depth: usize, embed_dim: i64, num_heads: i64) -> Self {
+        let patch_embed = PatchEmbed::new(&vs / "patch_embed", IMG_SIZE, PATCH_SIZE, 3, embed_dim);
+        let cls_token = vs.var("cls_token", &[1, 1, embed_dim], nn::Init::Const(0.));
+        let num_tokens = 1;
+        let pos_embed = vs.var(
+            "cls_token",
+            &[1, patch_embed.num_patches + num_tokens, embed_dim],
+            nn::Init::Const(0.),
+        );
         let head = nn::linear(&vs / "head", 0, 0, Default::default());
-        Self { head }
+        let norm = nn::layer_norm(&vs / "norm", vec![embed_dim], Default::default());
+        let blocks =
+            (0..depth).map(|i| Block::new(&vs / "blocks" / i, embed_dim, num_heads)).collect();
+        Self { patch_embed, cls_token, pos_embed, blocks, norm, head }
+    }
+
+    fn interpolate_pos_encoding(&self, xs: &Tensor, w: i64, h: i64) -> Tensor {
+        let npatch = xs.size()[1] - 1;
+        let n = self.pos_embed.size()[1] - 1;
+        let sqrt_n = (n as f64).sqrt();
+        if npatch == n && w == h {
+            return xs.shallow_clone();
+        }
+        let class_pos_embed = self.pos_embed.i((.., 0));
+        let patch_pos_embed = self.pos_embed.i((.., 1..));
+        let dim = *xs.size().last().unwrap();
+        let (w0, h0) = ((w / PATCH_SIZE) as f64 + 0.1, (h / PATCH_SIZE) as f64 + 0.1);
+        let patch_pos_embed = patch_pos_embed
+            .reshape([1, sqrt_n as i64, sqrt_n as i64, dim])
+            .permute([0, 3, 1, 2])
+            .upsample_bicubic2d([], false, w0 / sqrt_n, h0 / sqrt_n);
+        let patch_pos_embed = patch_pos_embed.permute([0, 2, 3, 1]).reshape([1, -1, dim]);
+        Tensor::cat(&[class_pos_embed, patch_pos_embed], 1)
+    }
+
+    fn prepare_tokens_with_mask(&self, xs: &Tensor) -> Tensor {
+        let (b, _nc, w, h) = xs.size4().unwrap();
+        let xs = xs.apply(&self.patch_embed);
+        let xs = Tensor::concat(&[self.cls_token.expand([b, -1, -1], false), xs], 1);
+        &xs + &self.interpolate_pos_encoding(&xs, w, h)
+    }
+}
+
+impl nn::Module for DinoVisionTransformer {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let mut xs = self.prepare_tokens_with_mask(xs);
+        for blk in self.blocks.iter() {
+            xs = xs.apply(blk)
+        }
+        let xs = xs.apply(&self.norm);
+        let xs_norm_clstoken = xs.i((.., 0));
+        xs_norm_clstoken.apply(&self.head)
     }
 }
