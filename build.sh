1897cdad991c922f8171c12d853312dc6a5ad4f9
Michael Scheuner <michael.scheuner@kenbun.de>
2023-08-18 14:14:02 +0200

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+#-e     When this option is on, if a simple command fails for any of the reasons listed in Consequences of
+#       Shell  Errors or returns an exit status value >0, and is not part of the compound list following a
+#       while, until, or if keyword, and is not a part of an AND  or  OR  list,  and  is  not  a  pipeline
+#       preceded by the ! reserved word, then the shell shall immediately exit.
+#-u     The shell shall write a message to standard error when it tries to expand a variable that  is  not
+#       set and immediately exit. An interactive shell shall not exit.
+#-x     The  shell shall write to standard error a trace for each command after it expands the command and
+#       before it executes it. It is unspecified whether the command that turns tracing off is traced.
+
+
+cargo fmt --all -- --check \
+&& cargo clippy --all --all-features -- -D warnings \
+&& cargo test --all --all-features && cargo build --release
