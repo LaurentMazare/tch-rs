7d6ac77058e391e93fb58b8ec98df1478c093764
Juho Ha <juhoha@furiosa.ai>
2024-09-05 09:00:07 +0900

    Fix clippy error

--- examples/python-extension/src/lib.rs
+++ examples/python-extension/src/lib.rs
@@ -3,7 +3,7 @@ use pyo3_tch::{wrap_tch_err, PyTensor};
 
 #[pyfunction]
 fn add_one(tensor: PyTensor) -> PyResult<PyTensor> {
-    let tensor = tensor.f_add_scalar(1.0).map_err(wrap_tch_err)?;
+    let tensor = tensor.f_add_scalar(1.0, 1.0).map_err(wrap_tch_err)?;
     Ok(PyTensor(tensor))
 }
 
